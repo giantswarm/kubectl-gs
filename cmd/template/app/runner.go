5e82194012e812dee110423ee6258fb9079d426f
pipo02mix <fernando@giantswarm.io>
2020-04-09 00:41:17 +0200

    Simplify app template generation

--- cmd/template/app/runner.go
+++ cmd/template/app/runner.go
@@ -40,85 +40,33 @@ func (r *runner) Run(cmd *cobra.Command, args []string) error {
 }
 
 func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
-	var kubeConfigSecretCRYaml []byte
-	var userConfigSecretCRYaml []byte
-	var userConfigConfigMapCRYaml []byte
-	var kubeSecretCR *v1.Secret
 	var userSecretCR *v1.Secret
 	var userConfigMapCR *v1.ConfigMap
+	var userConfigConfigMapCRYaml []byte
+	var userConfigSecretCRYaml []byte
 	var err error
-	appAssetID := key.GenerateID()
 
-	if r.flag.KubeConfigSecret != "" {
-		kubeConfigSecretData, err := key.ReadSecretYamlFromFile(afero.NewOsFs(), r.flag.Secret)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		secretConfig := app.SecretConfig{
-			Data:      kubeConfigSecretData,
-			Name:      key.GenerateAssetName(r.flag.Name, "kubeconfig", appAssetID),
-			Namespace: r.flag.Namespace,
-		}
-		kubeSecretCR, err = app.NewSecretCR(secretConfig)
-
-		kubeConfigSecretCRYaml, err = yaml.Marshal(kubeSecretCR)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-
-	var configMapData string
-	if r.flag.ConfigMap != "" {
-		configMapData, err = key.ReadConfigMapYamlFromFile(afero.NewOsFs(), r.flag.ConfigMap)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-	configMapConfig := app.ConfigMapConfig{
-		Data:      configMapData,
-		Name:      key.GenerateAssetName(r.flag.Name, appAssetID),
+	appConfig := app.Config{
+		Catalog:   r.flag.Catalog,
+		Name:      r.flag.Name,
 		Namespace: r.flag.Namespace,
-	}
-	configmapCR, err := app.NewConfigmapCR(configMapConfig)
-
-	configmapCRYaml, err := yaml.Marshal(configmapCR)
-	if err != nil {
-		return microerror.Mask(err)
+		Cluster:   r.flag.Cluster,
+		Version:   r.flag.Version,
 	}
 
-	var secretData map[string][]byte
-	if r.flag.Secret != "" {
-		secretData, err = key.ReadSecretYamlFromFile(afero.NewOsFs(), r.flag.Secret)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-
-	secretConfig := app.SecretConfig{
-		Data:      secretData,
-		Name:      key.GenerateAssetName(r.flag.Name, appAssetID),
-		Namespace: r.flag.Namespace,
-	}
-	secretCR, err := app.NewSecretCR(secretConfig)
-
-	secretCRYaml, err := yaml.Marshal(secretCR)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	{
-		userConfigSecretData, err := key.ReadSecretYamlFromFile(afero.NewOsFs(), r.flag.Secret)
+	if r.flag.flagUserSecret != "" {
+		userConfigSecretData, err := key.ReadSecretYamlFromFile(afero.NewOsFs(), r.flag.flagUserSecret)
 		if err != nil {
 			return microerror.Mask(err)
 		}
 
 		secretConfig := app.SecretConfig{
 			Data:      userConfigSecretData,
-			Name:      key.GenerateAssetName(r.flag.Name, "userconfig", appAssetID),
-			Namespace: r.flag.Namespace,
+			Name:      key.GenerateAssetName(r.flag.Name, "userconfig", r.flag.Cluster),
+			Namespace: r.flag.Cluster,
 		}
 		userSecretCR, err = app.NewSecretCR(secretConfig)
+		appConfig.UserConfigSecretName = userSecretCR.GetName()
 
 		userConfigSecretCRYaml, err = yaml.Marshal(userSecretCR)
 		if err != nil {
@@ -126,7 +74,7 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		}
 	}
 
-	{
+	if r.flag.flagUserConfigMap != "" {
 		var configMapData string
 		if r.flag.flagUserConfigMap != "" {
 			configMapData, err = key.ReadConfigMapYamlFromFile(afero.NewOsFs(), r.flag.flagUserConfigMap)
@@ -136,10 +84,11 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		}
 		configMapConfig := app.ConfigMapConfig{
 			Data:      configMapData,
-			Name:      key.GenerateAssetName(r.flag.Name, "userconfig", appAssetID),
-			Namespace: r.flag.Namespace,
+			Name:      key.GenerateAssetName(r.flag.Name, "userconfig", r.flag.Cluster),
+			Namespace: r.flag.Cluster,
 		}
 		userConfigMapCR, err = app.NewConfigmapCR(configMapConfig)
+		appConfig.UserConfigConfigMapName = userConfigMapCR.GetName()
 
 		userConfigConfigMapCRYaml, err = yaml.Marshal(userConfigMapCR)
 		if err != nil {
@@ -147,17 +96,6 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		}
 	}
 
-	appConfig := app.Config{
-		Catalog:                 r.flag.Catalog,
-		ID:                      appAssetID,
-		KubeConfigContext:       r.flag.KubeConfigContext,
-		KubeConfigSecretName:    kubeSecretCR.GetName(),
-		UserConfigSecretName:    userSecretCR.GetName(),
-		UserConfigConfigMapName: userConfigMapCR.GetName(),
-		Name:                    r.flag.Name,
-		Namespace:               r.flag.Namespace,
-		Version:                 r.flag.Version,
-	}
 	appCR, err := app.NewAppCR(appConfig)
 	if err != nil {
 		return microerror.Mask(err)
@@ -170,20 +108,14 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 
 	type AppCROutput struct {
 		AppCR                 string
-		ConfigmapCR           string
-		KubeConfigSecretCR    string
 		UserConfigSecretCR    string
 		UserConfigConfigMapCR string
-		SecretCR              string
 	}
 
 	appCROutput := AppCROutput{
 		AppCR:                 string(appCRYaml),
-		ConfigmapCR:           string(configmapCRYaml),
-		KubeConfigSecretCR:    string(kubeConfigSecretCRYaml),
 		UserConfigConfigMapCR: string(userConfigConfigMapCRYaml),
 		UserConfigSecretCR:    string(userConfigSecretCRYaml),
-		SecretCR:              string(secretCRYaml),
 	}
 
 	t := template.Must(template.New("appCatalogCR").Parse(key.AppCRTemplate))
