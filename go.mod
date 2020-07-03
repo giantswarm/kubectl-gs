e93a969866640ccc315bbc131a537584cef7c1bf
axbarsan <mnlk123@yahoo.com>
2020-07-03 17:36:56 +0200

    Add OIDC package

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/giantswarm/kubectl-gs
 go 1.13
 
 require (
+	github.com/coreos/go-oidc v2.1.0+incompatible
 	github.com/ghodss/yaml v1.0.0
 	github.com/giantswarm/apiextensions v0.4.0
 	github.com/giantswarm/microerror v0.2.0
@@ -12,6 +13,7 @@ require (
 	github.com/spf13/afero v1.2.2
 	github.com/spf13/cobra v0.0.7
 	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/appengine v1.6.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200121175148-a6ecf24a6d71
 	k8s.io/api v0.17.2
---
+++ pkg/oidc/oidc.go
@@ -0,0 +1,142 @@
+package oidc
+
+import (
+	"context"
+	"strings"
+
+	gooidc "github.com/coreos/go-oidc"
+	"github.com/giantswarm/microerror"
+	"golang.org/x/oauth2"
+)
+
+type Authenticator struct {
+	provider     gooidc.Provider
+	clientConfig oauth2.Config
+	challenge    string
+}
+
+type UserInfo struct {
+	Email         string
+	EmailVerified bool
+	IDToken       string
+	RefreshToken  string
+	IssuerURL     string
+	Username      string
+	Groups        []string
+	ClientID      string
+	ClientSecret  string
+}
+
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	Issuer       string
+	RedirectURL  string
+	AuthScopes   []string
+}
+
+type Claims struct {
+	Email    string   `json:"email"`
+	Verified bool     `json:"email_verified"`
+	Groups   []string `json:"groups"`
+}
+
+func New(ctx context.Context, c Config) (*Authenticator, error) {
+	provider, err := gooidc.NewProvider(ctx, c.Issuer)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	oauthConfig := oauth2.Config{
+		ClientID:     c.ClientID,
+		ClientSecret: c.ClientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  c.RedirectURL,
+		Scopes:       c.AuthScopes,
+	}
+
+	challenge, err := GenerateChallenge()
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	a := &Authenticator{
+		provider:     *provider,
+		clientConfig: oauthConfig,
+		challenge:    challenge,
+	}
+
+	return a, nil
+}
+
+func (a *Authenticator) GetAuthURL() string {
+	return a.clientConfig.AuthCodeURL(a.challenge, oauth2.AccessTypeOffline)
+}
+
+func (a *Authenticator) HandleIssuerResponse(ctx context.Context, challenge string, code string) (UserInfo, error) {
+	var err error
+
+	if challenge != a.challenge {
+		return UserInfo{}, microerror.Mask(invalidChallengeError)
+	}
+
+	var token *oauth2.Token
+	{
+		// Convert the authorization code into a token.
+		token, err = a.clientConfig.Exchange(ctx, code)
+		if err != nil {
+			return UserInfo{}, microerror.Mask(err)
+		}
+	}
+
+	var rawIDToken string
+	{
+		var ok bool
+		// Generate the ID Token.
+		rawIDToken, ok = token.Extra("id_token").(string)
+		if !ok {
+			return UserInfo{}, microerror.Mask(err)
+		}
+	}
+
+	var idToken *gooidc.IDToken
+	{
+		// Verify if ID Token is valid.
+		oidcConfig := &gooidc.Config{
+			ClientID: a.clientConfig.ClientID,
+		}
+
+		idToken, err = a.provider.Verifier(oidcConfig).Verify(ctx, rawIDToken)
+		if err != nil {
+			return UserInfo{}, microerror.Mask(err)
+		}
+	}
+
+	claims := Claims{}
+	{
+		// Get the user's info.
+		err = idToken.Claims(&claims)
+		if err != nil {
+			return UserInfo{}, microerror.Mask(err)
+		}
+	}
+
+	var username string
+	{
+		username = strings.Split(claims.Email, "@")[0]
+	}
+
+	info := UserInfo{
+		ClientID:      a.clientConfig.ClientID,
+		ClientSecret:  a.clientConfig.ClientSecret,
+		Email:         claims.Email,
+		EmailVerified: claims.Verified,
+		IDToken:       rawIDToken,
+		RefreshToken:  token.RefreshToken,
+		IssuerURL:     idToken.Issuer,
+		Username:      username,
+		Groups:        claims.Groups,
+	}
+
+	return info, nil
+}
