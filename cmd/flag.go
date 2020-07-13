5bec60bdfc3411d8f0c9a67720371fce39642d49
axbarsan <mnlk123@yahoo.com>
2020-07-13 18:35:16 +0200

    Ignore flag value

--- cmd/flag.go
+++ cmd/flag.go
@@ -5,16 +5,15 @@ import (
 )
 
 const (
-	flagDebugMode = "debug"
+	flagDebug = "debug"
 )
 
 type flag struct {
-	debugMode bool
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	// This value is ignored. The real value is handled inside 'main.go'.
-	cmd.PersistentFlags().BoolVar(&f.debugMode, flagDebugMode, false, "Toggle debug mode, for seeing full error output.")
+	cmd.PersistentFlags().Bool(flagDebug, false, "Toggle debug mode, for seeing full error output.")
 }
 
 func (f *flag) Validate() error {
