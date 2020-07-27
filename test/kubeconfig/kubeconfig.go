70a9c66e0878be54b4771ca0382584812ea53e07
axbarsan <mnlk123@yahoo.com>
2020-07-27 18:17:14 +0200

    Add fake kubeconfig for testing

---
+++ test/kubeconfig/kubeconfig.go
@@ -0,0 +1,36 @@
+package kubeconfig
+
+import (
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+func CreateFakeKubeConfig() clientcmd.ClientConfig {
+	config := createValidTestConfig()
+	clientBuilder := clientcmd.NewNonInteractiveClientConfig(*config, "clean", &clientcmd.ConfigOverrides{}, nil)
+
+	return clientBuilder
+}
+
+func createValidTestConfig() *clientcmdapi.Config {
+	const (
+		server = "https://anything.com:8080"
+		token  = "the-token"
+	)
+
+	config := clientcmdapi.NewConfig()
+	config.Clusters["clean"] = &clientcmdapi.Cluster{
+		Server: server,
+	}
+	config.AuthInfos["clean"] = &clientcmdapi.AuthInfo{
+		Token: token,
+	}
+	config.Contexts["clean"] = &clientcmdapi.Context{
+		Cluster:   "clean",
+		AuthInfo:  "clean",
+		Namespace: "default",
+	}
+	config.CurrentContext = "clean"
+
+	return config
+}
