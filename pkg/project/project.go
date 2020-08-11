5e43db11e78a4fe46affafd0e1b1970563b87f99
GitHub Action <action@github.com>
2020-08-11 17:01:08 +0000

    Bump version to 0.6.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kubectl-gs"
 	source      = "https://github.com/giantswarm/kubectl-gs"
-	version     = "0.6.0"
+	version     = "0.6.1-dev"
 )
 
 func Description() string {
