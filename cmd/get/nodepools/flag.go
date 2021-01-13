76262a7501817c61d539770209e381d845eb6572
Adrian Barsan <mnlk123@yahoo.com>
2021-01-13 13:41:30 +0100

    Remove extra file and rename tenant cluster to workload cluster

--- cmd/get/nodepools/flag.go
+++ cmd/get/nodepools/flag.go
@@ -20,7 +20,7 @@ type flag struct {
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().BoolVarP(&f.AllNamespaces, flagAllNamespaces, "A", false, "If present, list the requested object(s) across all namespaces. Namespace in current context is ignored even if specified with --namespace.")
-	cmd.Flags().StringVarP(&f.ClusterID, flagClusterID, "c", "", "If present, list the node pools that belong to this given tenant cluster.")
+	cmd.Flags().StringVarP(&f.ClusterID, flagClusterID, "c", "", "If present, list the node pools that belong to the given workload cluster.")
 
 	f.config = genericclioptions.NewConfigFlags(true)
 	f.print = genericclioptions.NewPrintFlags("")
