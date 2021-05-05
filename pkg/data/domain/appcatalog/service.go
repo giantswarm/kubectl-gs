7bbedb00cec342a7e360bd14fdd9e291127d16ac
Ross Fairbanks <rossf7@users.noreply.github.com>
2021-05-05 10:03:40 +0200

    Refactor appcatalog data service to return runtime objects (#330)
    * Refactor app data service to allow returning runtime objects
    
    * Refactor appcatalog data service to allow returning runtime objects
    
    * Handle invalid type error
    
    * Return both appcatalog CR and appcatalogentries
    
    * Tidy up label selectors
    
    * Remove else
    
    * Address review comments

--- pkg/data/domain/appcatalog/service.go
+++ pkg/data/domain/appcatalog/service.go
@@ -2,10 +2,14 @@ package appcatalog
 
 import (
 	"context"
+	"fmt"
 
 	applicationv1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/microerror"
-	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/labels"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/kubectl-gs/pkg/data/client"
 )
@@ -35,17 +39,105 @@ func New(config Config) (Interface, error) {
 	return s, nil
 }
 
-// Get fetches an AppCatalog CR by name.
-func (s *Service) Get(ctx context.Context, options GetOptions) (*applicationv1alpha1.AppCatalog, error) {
-	objKey := runtimeClient.ObjectKey{
-		Name: options.Name,
+// Get fetches a list of appcatalog CRs optionally filtered by name.
+func (s *Service) Get(ctx context.Context, options GetOptions) (Resource, error) {
+	var resource Resource
+	var err error
+
+	if len(options.Name) > 0 {
+		resource, err = s.getByName(ctx, options.Name)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		return resource, nil
 	}
 
-	appcatalog := &applicationv1alpha1.AppCatalog{}
-	err := s.client.K8sClient.CtrlClient().Get(ctx, objKey, appcatalog)
+	resource, err = s.getAll(ctx, options.LabelSelector)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
 
-	return appcatalog, nil
+	return resource, nil
+}
+
+func (s *Service) getAll(ctx context.Context, labelSelector labels.Selector) (Resource, error) {
+	appCatalogCollection := &Collection{}
+	var err error
+
+	{
+		lo := &runtimeclient.ListOptions{
+			LabelSelector: labelSelector,
+		}
+
+		appCatalogs := &applicationv1alpha1.AppCatalogList{}
+		{
+			err = s.client.K8sClient.CtrlClient().List(ctx, appCatalogs, lo)
+			if apimeta.IsNoMatchError(err) {
+				return nil, microerror.Mask(noMatchError)
+			} else if err != nil {
+				return nil, microerror.Mask(err)
+			} else if len(appCatalogs.Items) == 0 {
+				return nil, microerror.Mask(noResourcesError)
+			}
+		}
+
+		for _, catalog := range appCatalogs.Items {
+			a := AppCatalog{
+				CR: catalog.DeepCopy(),
+			}
+			appCatalogCollection.Items = append(appCatalogCollection.Items, a)
+		}
+	}
+
+	return appCatalogCollection, nil
+}
+
+func (s *Service) getByName(ctx context.Context, name string) (Resource, error) {
+	var err error
+
+	var selector labels.Selector
+	{
+		label := fmt.Sprintf("application.giantswarm.io/catalog=%s", name)
+		selector, err = labels.Parse(label)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	appCatalogCR := &applicationv1alpha1.AppCatalog{}
+	{
+		err = s.client.K8sClient.CtrlClient().Get(ctx, runtimeclient.ObjectKey{
+			Name: name,
+		}, appCatalogCR)
+		if apierrors.IsNotFound(err) {
+			return nil, microerror.Mask(notFoundError)
+		} else if apimeta.IsNoMatchError(err) {
+			return nil, microerror.Mask(noMatchError)
+		} else if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	entries := &applicationv1alpha1.AppCatalogEntryList{}
+	{
+		lo := &runtimeclient.ListOptions{
+			LabelSelector: selector,
+		}
+		err = s.client.K8sClient.CtrlClient().List(ctx, entries, lo)
+		if apimeta.IsNoMatchError(err) {
+			return nil, microerror.Mask(noMatchError)
+		} else if err != nil {
+			return nil, microerror.Mask(err)
+		} else if len(entries.Items) == 0 {
+			return nil, microerror.Mask(noResourcesError)
+		}
+	}
+
+	appCatalog := &AppCatalog{
+		CR:      appCatalogCR.DeepCopy(),
+		Entries: entries,
+	}
+
+	return appCatalog, nil
 }
--- pkg/data/domain/appcatalog/spec.go
+++ pkg/data/domain/appcatalog/spec.go
@@ -4,16 +4,71 @@ import (
 	"context"
 
 	applicationv1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
 )
+
+// AppCatalog abstracts away the custom resource so it can be returned as a runtime
+// object or a typed custom resource.
+type AppCatalog struct {
+	CR      *applicationv1alpha1.AppCatalog
+	Entries *applicationv1alpha1.AppCatalogEntryList
+}
+
+// Collection wraps a list of appcatalogs.
+type Collection struct {
+	Items []AppCatalog
+}
 
 // GetOptions are the parameters that the Get method takes.
 type GetOptions struct {
-	Name string
+	LabelSelector labels.Selector
+	Name          string
+}
+
+type Resource interface {
+	Object() runtime.Object
 }
 
 // Interface represents the contract for the appcatalog data service.
 // Using this instead of a regular 'struct' makes mocking the
 // service in tests much simpler.
 type Interface interface {
-	Get(context.Context, GetOptions) (*applicationv1alpha1.AppCatalog, error)
+	Get(context.Context, GetOptions) (Resource, error)
+}
+
+func (a *AppCatalog) Object() runtime.Object {
+	if a.CR != nil {
+		return a.CR
+	}
+	if a.Entries != nil {
+		return a.Entries
+	}
+
+	return nil
+}
+
+func (cc *Collection) Object() runtime.Object {
+	list := &metav1.List{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "List",
+			APIVersion: "v1",
+		},
+		ListMeta: metav1.ListMeta{},
+	}
+
+	for _, item := range cc.Items {
+		obj := item.Object()
+		if obj == nil {
+			continue
+		}
+
+		raw := runtime.RawExtension{
+			Object: obj,
+		}
+		list.Items = append(list.Items, raw)
+	}
+
+	return list
 }
