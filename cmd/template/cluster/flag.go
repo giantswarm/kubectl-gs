8b154a855017ed0f0a0e352f58ee0f5319bb48d4
Pau Rosello <paurosello@gmail.com>
2021-05-07 13:33:01 +0200

    Update limits

--- cmd/template/cluster/flag.go
+++ cmd/template/cluster/flag.go
@@ -112,7 +112,7 @@ func (f *flag) Validate() error {
 		// Validate Master AZs.
 		switch f.Provider {
 		case key.ProviderAWS:
-			if len(f.MasterAZ) <= 0 && len(f.MasterAZ) > 3 {
+			if len(f.MasterAZ) < 1 && len(f.MasterAZ) > 3 {
 				return microerror.Maskf(invalidFlagError, "--%s must be between one and three availability zone names", flagMasterAZ)
 			}
 			if !unique.StringsAreUnique(f.MasterAZ) {
