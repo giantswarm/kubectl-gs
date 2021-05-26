500959ef486338349b724ea07d7cf3b05ccc116f
Ross Fairbanks <rossf7@users.noreply.github.com>
2021-05-26 10:34:27 +0200

    Only set configmap or secret in template appcatalog if values are provided (#363)
    * Only set configmap or secret in template appcatalog if values are provided
    
    * Remove debug

--- cmd/template/appcatalog/runner.go
+++ cmd/template/appcatalog/runner.go
@@ -11,10 +11,10 @@ import (
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
-
-	appcatalog "github.com/giantswarm/kubectl-gs/pkg/template/appcatalog"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/kubectl-gs/internal/key"
+	templateappcatalog "github.com/giantswarm/kubectl-gs/pkg/template/appcatalog"
 )
 
 type runner struct {
@@ -41,15 +41,66 @@ func (r *runner) Run(cmd *cobra.Command, args []string) error {
 }
 
 func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
-	config := appcatalog.Config{
+	var configmapCRYaml []byte
+	var secretCRYaml []byte
+	var err error
+
+	config := templateappcatalog.Config{
 		Description: r.flag.Description,
 		LogoURL:     r.flag.LogoURL,
 		ID:          key.GenerateID(),
 		Name:        r.flag.Name,
+		Namespace:   metav1.NamespaceDefault,
 		URL:         r.flag.URL,
 	}
 
-	appCatalogCR, err := appcatalog.NewAppCatalogCR(config)
+	if r.flag.ConfigMap != "" {
+		var configMapData string
+
+		configMapData, err = key.ReadConfigMapYamlFromFile(afero.NewOsFs(), r.flag.ConfigMap)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		config.CatalogConfigMapName = key.GenerateAssetName(r.flag.Name, config.ID)
+		configmapCR, err := templateappcatalog.NewConfigmapCR(config, configMapData)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		configmapCRYaml, err = yaml.Marshal(configmapCR)
+		if err != nil {
+			return microerror.Maskf(unmashalToMapFailedError, err.Error())
+		}
+	}
+
+	if r.flag.Secret != "" {
+		var secretData []byte
+
+		secretData, err = key.ReadSecretYamlFromFile(afero.NewOsFs(), r.flag.Secret)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		config.CatalogSecretName = key.GenerateAssetName(r.flag.Name, config.ID)
+		secretCR, err := templateappcatalog.NewSecretCR(config, secretData)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		secretCRYaml, err = yaml.Marshal(secretCR)
+		if err != nil {
+			return microerror.Maskf(unmashalToMapFailedError, err.Error())
+		}
+	}
+
+	type AppCatalogCROutput struct {
+		AppCatalogCR string
+		ConfigmapCR  string
+		SecretCR     string
+	}
+
+	appCatalogCR, err := templateappcatalog.NewAppCatalogCR(config)
 	if err != nil {
 		return microerror.Mask(err)
 	}
@@ -57,46 +108,6 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	appCatalogCRYaml, err := yaml.Marshal(appCatalogCR)
 	if err != nil {
 		return microerror.Mask(err)
-	}
-
-	var configMapData string
-	if r.flag.ConfigMap != "" {
-		configMapData, err = key.ReadConfigMapYamlFromFile(afero.NewOsFs(), r.flag.ConfigMap)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-	configmapCR, err := appcatalog.NewConfigmapCR(config, configMapData)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	configmapCRYaml, err := yaml.Marshal(configmapCR)
-	if err != nil {
-		return microerror.Maskf(unmashalToMapFailedError, err.Error())
-	}
-
-	var secretData []byte
-	if r.flag.Secret != "" {
-		secretData, err = key.ReadSecretYamlFromFile(afero.NewOsFs(), r.flag.Secret)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-	secretCR, err := appcatalog.NewSecretCR(config, secretData)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	secretCRYaml, err := yaml.Marshal(secretCR)
-	if err != nil {
-		return microerror.Maskf(unmashalToMapFailedError, err.Error())
-	}
-
-	type AppCatalogCROutput struct {
-		AppCatalogCR string
-		ConfigmapCR  string
-		SecretCR     string
 	}
 
 	appCatalogCROutput := AppCatalogCROutput{
