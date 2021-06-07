243523864256a046c0ff671c8fe1bb4b9797b6a4
Marian Steinbach <marian@giantswarm.io>
2021-06-07 17:00:13 +0200

    Fixes after review

--- cmd/template/cluster/flag.go
+++ cmd/template/cluster/flag.go
@@ -21,7 +21,7 @@ const (
 	// Common.
 	flagClusterID      = "cluster-id"
 	flagControlPlaneAZ = "control-plane-az"
-	flagMasterAZ       = "master-az" // TODO: Remove some time after August 2020
+	flagMasterAZ       = "master-az" // TODO: Remove some time after August 2021
 	flagName           = "name"
 	flagOutput         = "output"
 	flagOwner          = "owner"
@@ -57,21 +57,21 @@ func (f *flag) Init(cmd *cobra.Command) {
 	// Common.
 	cmd.Flags().StringVar(&f.ClusterID, flagClusterID, "", "User-defined cluster ID.")
 	cmd.Flags().StringSliceVar(&f.ControlPlaneAZ, flagControlPlaneAZ, []string{}, "Availability zone(s) to use by control plane nodes.")
-	cmd.Flags().StringSliceVar(&f.MasterAZ, flagMasterAZ, []string{}, "Replaced by --control-plane-az.")
+	cmd.Flags().StringSliceVar(&f.MasterAZ, flagMasterAZ, nil, "Replaced by --control-plane-az.")
 	cmd.Flags().StringVar(&f.Name, flagName, "", "Workload cluster name.")
 	cmd.Flags().StringVar(&f.Output, flagOutput, "", "File path for storing CRs.")
 	cmd.Flags().StringVar(&f.Owner, flagOwner, "", "Workload cluster owner organization.")
 	cmd.Flags().StringVar(&f.Release, flagRelease, "", "Workload cluster release. If not given, this remains empty for defaulting to the most recent one via the Management API.")
 	cmd.Flags().StringSliceVar(&f.Label, flagLabel, nil, "Workload cluster label.")
 
-	// TODO: Remove the flag completely some time after August 2020
+	// TODO: Remove the flag completely some time after August 2021
 	_ = cmd.Flags().MarkDeprecated(flagMasterAZ, "please use --control-plane-az.")
 }
 
 func (f *flag) Validate() error {
 	var err error
 
-	// TODO: Remove the flag completely some time after August 2020
+	// TODO: Remove the flag completely some time after August 2021
 	if len(f.MasterAZ) > 0 && len(f.ControlPlaneAZ) > 0 {
 		return microerror.Maskf(invalidFlagError, "--control-plane-az and --master-az cannot be combined")
 	}
