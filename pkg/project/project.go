4b75d84a37099c49d2667e8d4a39de93b40fb6d3
Fernando Ripoll <fernando@giantswarm.io>
2021-06-15 11:15:06 +0200

    Master#release#v1.29.0 (#369)
    * Add AWS subnets
    
    * Fix error message
    
    * Set good name for mahcine deployment flag
    
    * Only in AWS CR
    
    * Set right validation
    
    * Set right validation message
    
    * Add changelog entry
    
    * Add missing dot
    
    * Release v1.29.0
    
    Co-authored-by: github-actions <action@github.com>

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kubectl-gs"
 	source      = "https://github.com/giantswarm/kubectl-gs"
-	version     = "1.28.1-dev"
+	version     = "1.29.0"
 )
 
 func Description() string {
