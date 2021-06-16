b9ee7493af2a93b692adf33233660792d7f059ae
GitHub Action <action@github.com>
2021-06-16 08:22:44 +0000

    Bump version to 1.29.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kubectl-gs"
 	source      = "https://github.com/giantswarm/kubectl-gs"
-	version     = "1.29.1"
+	version     = "1.29.2-dev"
 )
 
 func Description() string {
