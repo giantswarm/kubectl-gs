49d117b3c1cd5fc90bdc6104fd7074d2db2678b5
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2021-10-21 16:01:04 +0200

    Remove not needed printers

--- cmd/update/app/printer.go
+++ cmd/update/app/printer.go
@@ -1,10 +1,1 @@
 package app
-
-import (
-	"fmt"
-)
-
-func (r *runner) printNoMatchOutput(version string) {
-	fmt.Fprintf(r.stdout, "No AppCatalogEntry CR found for the given version: '%s'\n", version)
-	fmt.Fprintf(r.stdout, "Please make sure version you are requesting is available in the respective catalog\n")
-}
