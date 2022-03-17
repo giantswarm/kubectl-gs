a9befcd412e4db1f2debed33b5782d94963c443f
Ross Fairbanks <rossf7@users.noreply.github.com>
2022-03-17 11:55:39 +0100

    Default latest version from AppCatalogEntry

--- cmd/template/app/flag.go
+++ cmd/template/app/flag.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/giantswarm/microerror"
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 
 	"github.com/giantswarm/kubectl-gs/pkg/annotations"
 	"github.com/giantswarm/kubectl-gs/pkg/labels"
@@ -14,14 +15,14 @@ const (
 	flagAppName                    = "app-name"
 	flagCatalog                    = "catalog"
 	flagCatalogNamespace           = "catalog-namespace"
-	flagCluster                    = "cluster"
+	flagClusterName                = "cluster-name"
 	flagDefaultingEnabled          = "defaulting-enabled"
 	flagInCluster                  = "in-cluster"
 	flagName                       = "name"
-	flagNamespace                  = "namespace"
 	flagNamespaceConfigAnnotations = "namespace-annotations"
 	flagNamespaceConfigLabels      = "namespace-labels"
 	flagOrganization               = "organization"
+	flagTargetNamespace            = "target-namespace"
 	flagUserConfigMap              = "user-configmap"
 	flagUserSecret                 = "user-secret"
 	flagVersion                    = "version"
@@ -31,17 +32,19 @@ type flag struct {
 	AppName                        string
 	Catalog                        string
 	CatalogNamespace               string
-	Cluster                        string
+	ClusterName                    string
 	DefaultingEnabled              bool
 	InCluster                      bool
 	Name                           string
-	Namespace                      string
 	Organization                   string
+	TargetNamespace                string
 	Version                        string
 	flagNamespaceConfigAnnotations []string
 	flagNamespaceConfigLabels      []string
 	flagUserConfigMap              string
 	flagUserSecret                 string
+
+	config genericclioptions.RESTClientGetter
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
@@ -49,11 +52,11 @@ func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.Catalog, flagCatalog, "", "Catalog name where app is stored.")
 	cmd.Flags().StringVar(&f.CatalogNamespace, flagCatalogNamespace, "", "Catalog namespace where Catalog CR is stored if outside the default namespace.")
 	cmd.Flags().StringVar(&f.Name, flagName, "", "Name of the app in the Catalog.")
-	cmd.Flags().StringVar(&f.Namespace, flagNamespace, "", "Namespace where the app will be deployed.")
-	cmd.Flags().StringVar(&f.Cluster, flagCluster, "", "Name of the cluster the app will be deployed to.")
+	cmd.Flags().StringVar(&f.ClusterName, flagClusterName, "", "Name of the cluster the app will be deployed to.")
 	cmd.Flags().StringVar(&f.Organization, flagOrganization, "", "Workload cluster organization.")
 	cmd.Flags().BoolVar(&f.DefaultingEnabled, flagDefaultingEnabled, true, "Don't template fields that will be defaulted.")
-	cmd.Flags().BoolVar(&f.InCluster, flagInCluster, false, fmt.Sprintf("Deploy the app in the current management cluster rather than in a workload cluster. If this is set, --%s will be ignored.", flagCluster))
+	cmd.Flags().BoolVar(&f.InCluster, flagInCluster, false, fmt.Sprintf("Deploy the app in the current management cluster rather than in a workload cluster. If this is set, --%s will be ignored.", flagClusterName))
+	cmd.Flags().StringVar(&f.TargetNamespace, flagTargetNamespace, "", "Namespace where the app will be deployed.")
 	cmd.Flags().StringVar(&f.flagUserConfigMap, flagUserConfigMap, "", "Path to the user values configmap YAML file.")
 	cmd.Flags().StringVar(&f.flagUserSecret, flagUserSecret, "", "Path to the user secrets YAML file.")
 	cmd.Flags().StringVar(&f.Version, flagVersion, "", "App version to be installed.")
@@ -61,6 +64,12 @@ func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringSliceVar(&f.flagNamespaceConfigLabels, flagNamespaceConfigLabels, nil, "Namespace configuration labels in form key=value.")
 
 	_ = cmd.Flags().MarkHidden(flagOrganization)
+
+	f.config = genericclioptions.NewConfigFlags(true)
+
+	// Merging current command flags and config flags,
+	// to be able to override kubectl-specific ones.
+	f.config.(*genericclioptions.ConfigFlags).AddFlags(cmd.Flags())
 }
 
 func (f *flag) Validate() error {
@@ -70,14 +79,11 @@ func (f *flag) Validate() error {
 	if f.Name == "" {
 		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagName)
 	}
-	if f.Namespace == "" {
-		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagNamespace)
+	if !f.InCluster && f.ClusterName == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagClusterName)
 	}
-	if !f.InCluster && f.Cluster == "" {
-		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagCluster)
-	}
-	if f.Version == "" {
-		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagVersion)
+	if f.TargetNamespace == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagTargetNamespace)
 	}
 
 	_, err := labels.Parse(f.flagNamespaceConfigLabels)
--- cmd/template/app/runner.go
+++ cmd/template/app/runner.go
@@ -8,13 +8,17 @@ import (
 	"strings"
 	"text/template"
 
+	applicationv1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/cobra"
+	pkglabels "k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 
 	"github.com/giantswarm/kubectl-gs/internal/key"
 	"github.com/giantswarm/kubectl-gs/pkg/annotations"
+	"github.com/giantswarm/kubectl-gs/pkg/commonconfig"
 	"github.com/giantswarm/kubectl-gs/pkg/labels"
 	templateapp "github.com/giantswarm/kubectl-gs/pkg/template/app"
 )
@@ -47,9 +51,24 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	var userConfigSecretYaml []byte
 	var err error
 
+	commonConfig := commonconfig.New(r.flag.config)
+	c, err := commonConfig.GetClient(r.logger)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
 	appName := r.flag.AppName
 	if appName == "" {
 		appName = r.flag.Name
+	}
+
+	if r.flag.Version == "" {
+		version, err := getLatestVersion(ctx, c.CtrlClient(), r.flag.Name, r.flag.Catalog)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.flag.Version = version
 	}
 
 	// Since organization may be provided in a mixed-cased form,
@@ -60,11 +79,11 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		AppName:           appName,
 		Catalog:           r.flag.Catalog,
 		CatalogNamespace:  r.flag.CatalogNamespace,
-		Cluster:           r.flag.Cluster,
+		Cluster:           r.flag.ClusterName,
 		DefaultingEnabled: r.flag.DefaultingEnabled,
 		InCluster:         r.flag.InCluster,
 		Name:              r.flag.Name,
-		Namespace:         r.flag.Namespace,
+		Namespace:         r.flag.TargetNamespace,
 		Organization:      organization,
 		Version:           r.flag.Version,
 	}
@@ -73,18 +92,18 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	if r.flag.InCluster {
 		assetName = key.GenerateAssetName(appName, "userconfig")
 	} else {
-		assetName = key.GenerateAssetName(appName, "userconfig", r.flag.Cluster)
+		assetName = key.GenerateAssetName(appName, "userconfig", r.flag.ClusterName)
 	}
 
 	// If organization is passed to the command use it as the indicator for
 	// templating App CR in org namespace.
 	var namespace string
 	if r.flag.InCluster {
-		namespace = r.flag.Namespace
+		namespace = r.flag.TargetNamespace
 	} else if appConfig.Organization != "" {
 		namespace = fmt.Sprintf("org-%s", appConfig.Organization)
 	} else {
-		namespace = r.flag.Cluster
+		namespace = r.flag.ClusterName
 	}
 
 	if r.flag.flagUserSecret != "" {
@@ -155,4 +174,25 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	}
 
 	return nil
+}
+
+func getLatestVersion(ctx context.Context, ctrlClient client.Client, app, catalog string) (string, error) {
+	var catalogEntryList applicationv1alpha1.AppCatalogEntryList
+	err := ctrlClient.List(ctx, &catalogEntryList, &client.ListOptions{
+		LabelSelector: pkglabels.SelectorFromSet(map[string]string{
+			"app.kubernetes.io/name":            app,
+			"application.giantswarm.io/catalog": catalog,
+			"latest":                            "true",
+		}),
+		Namespace: "default",
+	})
+
+	if err != nil {
+		return "", microerror.Mask(err)
+	} else if len(catalogEntryList.Items) != 1 {
+		message := fmt.Sprintf("version not specified for %s and latest release couldn't be determined in %s catalog", app, catalog)
+		return "", microerror.Maskf(invalidFlagError, message)
+	}
+
+	return catalogEntryList.Items[0].Spec.Version, nil
 }
