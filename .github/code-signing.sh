00165097ef91de6d2827050b441b5e181afd16b6
Marian Steinbach <marian@giantswarm.io>
2022-04-19 19:42:57 +0200

    Update code-signing.sh

--- .github/code-signing.sh
+++ .github/code-signing.sh
@@ -3,7 +3,10 @@
 APPLICATION=$1
 VERSION=$2
 
-if [[ "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}" != "" ]]; then
+echo "APPLICATION=${APPLICATION}"
+echo "VERSION=${VERSION}"
+
+if [ "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}" = "" ]; then
 	echo "Signing the Windows binary"
 	mkdir -p certs
 	echo "${CODE_SIGNING_CERT_BUNDLE_BASE64}" | base64 -d > certs/code-signing.p12
@@ -21,4 +24,4 @@ if [[ "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}" != "" ]]; then
 		-in /mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe \
 		-out /mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64.exe \
 		-pass "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}"
-fi
+fi;
