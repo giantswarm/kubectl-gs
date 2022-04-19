6a6756171dd1f803fd45b319aa8b7afed3971c4b
Marian Steinbach <marian@giantswarm.io>
2022-04-19 18:42:36 +0200

    Escape quotes

--- Makefile.gen.go.mk
+++ Makefile.gen.go.mk
@@ -88,10 +88,10 @@ package-windows-amd64: $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-windows-amd64.z
 $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-windows-amd64.zip: DIR=$(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-windows-amd64
 $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-windows-amd64.zip: $(APPLICATION)-v$(VERSION)-windows-amd64.exe
 	@echo "====> $@"
-	@if [ "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}" != "" ]; then \
-		echo 'Signing the Windows binary'; \
+	@if [ \"${CODE_SIGNING_CERT_BUNDLE_PASSWORD}\" != \"\" ]; then \
+		echo \"Signing the Windows binary\"; \
 		mkdir -p certs; \
-		echo '${CODE_SIGNING_CERT_BUNDLE_BASE64}' | base64 -d > certs/code-signing.p12; \
+		echo \"${CODE_SIGNING_CERT_BUNDLE_BASE64}\" | base64 -d > certs/code-signing.p12; \
 		mv ${APPLICATION}-v${VERSION}-windows-amd64.exe ${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe; \
 		docker run --rm -ti \
 			-v ${PWD}/certs:/mnt/certs \
@@ -100,7 +100,7 @@ $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-windows-amd64.zip: $(APPLICATION)-v$(V
 			quay.io/giantswarm/signcode-util:1.1.1 \
 			sign \
 			-pkcs12 /mnt/certs/code-signing.p12 \
-			-n 'Giant Swarm CLI tool $(APPLICATION)' \
+			-n \"Giant Swarm CLI tool $(APPLICATION)\" \
 			-i https://github.com/giantswarm/$(APPLICATION) \
 			-t http://timestamp.digicert.com -verbose \
 			-in /mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe \
