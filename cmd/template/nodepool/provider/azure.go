f4e6af48598c9d3c93304a4e32de3dd699e931c6
Antonia von den Driesch <antonia@giantswarm.io>
2022-05-02 21:42:51 +0900

    remove some explicit typemetas

--- cmd/template/nodepool/provider/azure.go
+++ cmd/template/nodepool/provider/azure.go
@@ -172,10 +172,6 @@ func newAzureMachinePoolCR(config NodePoolCRsConfig) *capzexp.AzureMachinePool {
 	}
 
 	azureMp := &capzexp.AzureMachinePool{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "AzureMachinePool",
-			APIVersion: "exp.infrastructure.cluster.x-k8s.io/v1alpha3",
-		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      config.NodePoolName,
 			Namespace: config.Namespace,
