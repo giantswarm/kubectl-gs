47820430a7c673e66f9ad46b26cf06b54a992f86
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2022-06-20 10:07:24 +0200

    Putting some more changes

--- internal/gitops/structure/key.go
+++ internal/gitops/structure/key.go
@@ -1,13 +1,42 @@
 package structure
 
+import (
+	"fmt"
+)
+
 const (
 	fluxKustomizationAPIVersion = "kustomize.toolkit.fluxcd.io/v1beta2"
 	fluxKustomizationKind       = "Kustomization"
+	fluxSourceKind              = "GitRepository"
 
 	topLevelGitOpsDirectory     = "management-clusters"
 	topLevelKustomizationSuffix = "gitops"
+	clustersKustomizationPrefix = "clusters"
 
-	secretsDirectory        = "secrets"
-	sopsPublicKeysDirectory = ".sops.keys"
-	organizationsDirectory  = "organizations"
+	kustomizationNamespace = "default"
+
+	secretsDirectory          = "secrets"
+	sopsPublicKeysDirectory   = ".sops.keys"
+	organizationsDirectory    = "organizations"
+	workloadClustersDirectory = "workload-clusters"
 )
+
+func kustomizationFileName(name string) string {
+	return fmt.Sprintf("%s.yaml", name)
+}
+
+func kustomizationName(mc, wc string) string {
+	if wc == "" {
+		return fmt.Sprintf("%s-%s", mc, topLevelKustomizationSuffix)
+	} else {
+		return fmt.Sprintf("%s-%s-%s", mc, clustersKustomizationPrefix, wc)
+	}
+}
+
+func kustomizationPath(mc, org, wc string) string {
+	if wc == "" || org == "" {
+		return fmt.Sprintf("./%s/%s", topLevelGitOpsDirectory, mc)
+	} else {
+		return fmt.Sprintf("./%s/%s/%s/%s/%s/%s", topLevelGitOpsDirectory, mc, organizationsDirectory, org, workloadClustersDirectory, wc)
+	}
+}
