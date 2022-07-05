59eb61bddaac1c27bd197b4e4e271e9c04b853ff
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2022-07-05 18:27:11 +0200

    More fixing

--- internal/gitops/structure/templates/management-cluster/template.go
+++ internal/gitops/structure/templates/management-cluster/template.go
@@ -7,7 +7,7 @@ import (
 //go:embed management-cluster.yaml.tmpl
 var managementCluster string
 
-// GetManagementClusterTemplates merges .tmpl files for organization layer.
+// GetManagementClusterTemplates merges .tmpl files for management cluster layer.
 func GetManagementClusterTemplates() []string {
 	return []string{
 		managementCluster,
