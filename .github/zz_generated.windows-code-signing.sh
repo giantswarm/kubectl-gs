dfba1bad1d9bf028176aedfdab7cb82fa2150553
ArchitectBot <61872893+architectbot@users.noreply.github.com>
2022-07-15 08:20:35 +0200

    Align files (#858)
    Co-authored-by: github-actions <action@github.com>
    Co-authored-by: Antonia <56017655+anvddriesch@users.noreply.github.com>

--- .github/zz_generated.windows-code-signing.sh
+++ .github/zz_generated.windows-code-signing.sh
@@ -2,7 +2,7 @@
 
 # DO NOT EDIT. Generated with:
 #
-#    devctl@5.2.0
+#    devctl@5.8.0
 #
 
 APPLICATION=$1
@@ -14,14 +14,16 @@ echo "APPLICATION=${APPLICATION}"
 echo "VERSION=${VERSION}"
 echo "PWD=${PWD}"
 
+NO_CODE_SIGNING="Skipping Windows binary signing. In order to create a signed Windows binary, set the environment variables CODE_SIGNING_CERT_BUNDLE_PASSWORD and CODE_SIGNING_CERT_BUNDLE_BASE64."
+
 if [ "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}" = "" ]; then
-	echo "Variable CODE_SIGNING_CERT_BUNDLE_PASSWORD not set."
-	exit 1
+	echo "Variable CODE_SIGNING_CERT_BUNDLE_PASSWORD not set. ${NO_CODE_SIGNING}"
+	exit 0
 fi;
 
 if [ "${CODE_SIGNING_CERT_BUNDLE_BASE64}" = "" ]; then
-	echo "Variable CODE_SIGNING_CERT_BUNDLE_BASE64 not set."
-	exit 1
+	echo "Variable CODE_SIGNING_CERT_BUNDLE_BASE64 not set.  ${NO_CODE_SIGNING}"
+	exit 0
 fi;
 
 echo "Signing the Windows binary"
@@ -30,27 +32,27 @@ mkdir -p certs
 
 echo "${CODE_SIGNING_CERT_BUNDLE_BASE64}" | base64 -d > certs/code-signing.p12
 
-mv ${APPLICATION}-v${VERSION}-windows-amd64.exe ${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe
+mv "${APPLICATION}-v${VERSION}-windows-amd64.exe" "${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe"
 
 docker pull --quiet ${SIGNCODE_UTIL}
 
 docker run --rm \
-	-v ${PWD}/certs:/mnt/certs \
-	-v ${PWD}:/mnt/binaries \
+	-v "${PWD}/certs:/mnt/certs" \
+	-v "${PWD}:/mnt/binaries" \
 	${SIGNCODE_UTIL} \
 	sign \
 	-pkcs12 /mnt/certs/code-signing.p12 \
 	-n "Giant Swarm CLI tool ${APPLICATION}" \
-	-i https://github.com/giantswarm/${APPLICATION} \
+	-i "https://github.com/giantswarm/${APPLICATION}" \
 	-t http://timestamp.digicert.com -verbose \
-	-in /mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe \
-	-out /mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64.exe \
+	-in "/mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64-unsigned.exe" \
+	-out "/mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64.exe" \
 	-pass "${CODE_SIGNING_CERT_BUNDLE_PASSWORD}"
 
 echo "Verifying the signed binary"
 
 docker run --rm \
-	-v ${PWD}:/mnt/binaries \
+	-v "${PWD}:/mnt/binaries" \
 	${SIGNCODE_UTIL} \
 	verify \
-	/mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64.exe
+	"/mnt/binaries/${APPLICATION}-v${VERSION}-windows-amd64.exe"
