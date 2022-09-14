8d9a21a0fcfe24df01ca367f9174377af4a1b4eb
vvondruska <vondruska.vaclav@gmail.com>
2022-09-14 11:17:32 +0200

    Fixed usage instructions of gitops add app

--- cmd/gitops/add/app/command.go
+++ cmd/gitops/add/app/command.go
@@ -41,7 +41,7 @@ https://github.com/giantswarm/gitops-template/blob/main/docs/apps/add_appcr.md`
   --app hello-world-app \
   --catalog giantswarm \
   --name hello-world \
-  --namespace default \
+  --target-namespace default \
   --management-cluster demowc \
   --organization demoorg \
   --version 0.3.0 \
@@ -52,7 +52,7 @@ https://github.com/giantswarm/gitops-template/blob/main/docs/apps/add_appcr.md`
   --app hello-world-app \
   --catalog giantswarm \
   --name hello-world \
-  --namespace default \
+  --target-namespace default \
   --management-cluster demomc \
   --organization demoorg \
   --user-configmap /tmp/hello-world-app-values.yaml \
