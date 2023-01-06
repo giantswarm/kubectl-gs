381c019f5e8b1d8e52b6018021fb5a75c43650df
Laszlo Uveges <laszlo@giantswarm.io>
2023-01-06 09:40:52 +0100

    Get a working prototype for CAP[A,G,Z]
    O is omitted as not supported at the moment
    V is not testable at the moment (no such ready cluster)

--- cmd/gitops/add/base/runner.go
+++ cmd/gitops/add/base/runner.go
@@ -41,12 +41,37 @@ func (r *runner) Run(cmd *cobra.Command, args []string) error {
 func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
 	fmt.Println("Hello world!")
 
+	//err := r.writeCapaTemplate(ctx)
+	//if err != nil {
+	//	return microerror.Mask(err)
+	//}
+
+	err := r.writeCapgTemplate(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	//err := r.writeCapvTemplate(ctx)
+	//if err != nil {
+	//	return microerror.Mask(err)
+	//}
+
+	//err := r.writeCapzTemplate(ctx)
+	//if err != nil {
+	//	return microerror.Mask(err)
+	//}
+
+	fmt.Println("Good bye world!")
+	return nil
+}
+
+func (r *runner) writeCapaTemplate(ctx context.Context) error {
 	client, err := r.commonConfig.GetClient(r.logger)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	err = provider.WriteCAPATemplate(ctx, client, r.stdout, provider.ClusterConfig{
+	return provider.WriteCAPATemplate(ctx, client, r.stdout, provider.ClusterConfig{
 		Name:         "${cluster_name}",
 		Organization: "${organization}",
 		Namespace:    "${organization}",
@@ -59,10 +84,70 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 			DefaultAppsVersion: "${default_apps_version}",
 		},
 	})
+}
+
+func (r *runner) writeCapgTemplate(ctx context.Context) error {
+	client, err := r.commonConfig.GetClient(r.logger)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	fmt.Println("Good bye world!")
-	return nil
+	return provider.WriteGCPTemplate(ctx, client, r.stdout, provider.ClusterConfig{
+		Name:         "${cluster_name}",
+		Organization: "${organization}",
+		Namespace:    "${organization}",
+		App: provider.AppConfig{
+			ClusterCatalog:     "cluster",
+			ClusterVersion:     "${cluster_version}",
+			DefaultAppsCatalog: "cluster",
+			DefaultAppsVersion: "${default_apps_version}",
+		},
+	})
+}
+
+// Issues:
+// - no ready cluster for it to test with?
+//func (r *runner) writeCapvTemplate(ctx context.Context) error {
+//	client, err := r.commonConfig.GetClient(r.logger)
+//	if err != nil {
+//		return microerror.Mask(err)
+//	}
+//
+//	return provider.WriteVSphereTemplate(ctx, client, r.stdout, provider.ClusterConfig{
+//		Name:         "${cluster_name}",
+//		Organization: "${organization}",
+//		Namespace:    "${organization}",
+//		App: provider.AppConfig{
+//			ClusterCatalog:     "cluster",
+//			ClusterVersion:     "${cluster_version}",
+//			DefaultAppsCatalog: "cluster",
+//			DefaultAppsVersion: "${default_apps_version}",
+//		},
+//	})
+//}
+
+// Issues:
+//   - the organization name must be valid and exist on the MC that the command runs against
+//   - the cluster name must be valid: "lowercase RFC 1123 subdomain"
+func (r *runner) writeCapzTemplate(ctx context.Context) error {
+	client, err := r.commonConfig.GetClient(r.logger)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return provider.WriteCAPZTemplate(ctx, client, r.stdout, provider.ClusterConfig{
+		//Name: "${cluster_name}",
+		// needed because of: https://github.com/giantswarm/kubectl-gs/blob/43bcc221f67194ceee51965f2c7b485808726cf9/cmd/template/cluster/provider/common.go#L185
+		// so we would need to disable that or need a replacing logic, value must be a "lowercase RFC 1123 subdomain"
+		Name: "placeholder-cluster-name",
+		//Organization: "${organization}",
+		Organization: "test-philippe",
+		Namespace:    "${organization}",
+		App: provider.AppConfig{
+			ClusterCatalog:     "cluster",
+			ClusterVersion:     "${cluster_version}",
+			DefaultAppsCatalog: "cluster",
+			DefaultAppsVersion: "${default_apps_version}",
+		},
+	})
 }
