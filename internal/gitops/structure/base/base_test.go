4f34a3a5c1dcfd69cbc66f59d2e79530cdcb43fe
Laszlo Uveges <laszlo@giantswarm.io>
2023-01-09 17:35:50 +0100

    Add unit test for gitops add base command

---
+++ internal/gitops/structure/base/base_test.go
@@ -0,0 +1,101 @@
+package base
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/giantswarm/kubectl-gs/v2/internal/gitops/structure/common"
+)
+
+type FsObjectExpected struct {
+	RelativePath string
+	GoldenFile   string
+}
+
+func Test_NewManagementCluster(t *testing.T) {
+	testCases := []struct {
+		name            string
+		config          common.StructureConfig
+		expectedObjects []FsObjectExpected
+	}{
+		{
+			name: "flawless",
+			config: common.StructureConfig{
+				Provider: "capa",
+				ClusterBaseTemplates: common.ClusterBaseTemplates{
+					ClusterAppCr:      "foo:bar",
+					ClusterValues:     "foo:bar",
+					DefaultAppsAppCr:  "foo:bar",
+					DefaultAppsValues: "foo:bar",
+				},
+			},
+			expectedObjects: []FsObjectExpected{
+				{
+					RelativePath: "bases",
+				},
+				{
+					RelativePath: "bases/clusters",
+				},
+				{
+					RelativePath: "bases/clusters/capa",
+				},
+				{
+					RelativePath: "bases/clusters/capa/kustomization.yaml",
+					GoldenFile:   "testdata/expected/0-capa-kustomization.golden",
+				},
+				{
+					RelativePath: "bases/clusters/capa/cluster.yaml",
+					GoldenFile:   "testdata/expected/foo-bar.golden",
+				},
+				{
+					RelativePath: "bases/clusters/capa/cluster_config.yaml",
+					GoldenFile:   "testdata/expected/foo-bar.golden",
+				},
+				{
+					RelativePath: "bases/clusters/capa/default_apps.yaml",
+					GoldenFile:   "testdata/expected/foo-bar.golden",
+				},
+				{
+					RelativePath: "bases/clusters/capa/default_apps_config.yaml",
+					GoldenFile:   "testdata/expected/foo-bar.golden",
+				},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("case %d: %s", i, tc.name), func(t *testing.T) {
+			config, err := NewClusterBase(tc.config)
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err.Error())
+			}
+
+			if len(config.FsObjects) != len(tc.expectedObjects) {
+				t.Fatalf("expected %d, got: %d", len(tc.expectedObjects), len(config.FsObjects))
+			}
+
+			for i, e := range tc.expectedObjects {
+				if e.RelativePath != config.FsObjects[i].RelativePath {
+					t.Fatalf("expected path %s, got %s", e.RelativePath, config.FsObjects[i].RelativePath)
+				}
+
+				if e.GoldenFile == "" {
+					continue
+				}
+
+				expected, err := os.ReadFile(e.GoldenFile)
+				if err != nil {
+					t.Fatalf("unexpected error: %s", err.Error())
+				}
+
+				if !bytes.Equal(config.FsObjects[i].Data, expected) {
+					t.Fatalf("want matching files \n%s\n", cmp.Diff(string(expected), string(config.FsObjects[i].Data)))
+				}
+			}
+		})
+	}
+}
