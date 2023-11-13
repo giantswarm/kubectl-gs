cf88a23169006153869e3e9c8094819a0145cad8
taylorbot <dev@giantswarm.io>
2023-11-13 12:19:14 +0000

    Release v2.47.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kubectl-gs"
 	source      = "https://github.com/giantswarm/kubectl-gs"
-	version     = "2.46.1-dev"
+	version     = "2.47.0"
 )
 
 func Description() string {
