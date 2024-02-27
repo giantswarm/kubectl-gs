ee6306081769c21f4e3ad5be75465e07b4629219
Nick Jüttner <nick@giantswarm.io>
2024-02-27 12:50:21 +0100

    support nodepools for CAPZ,CAPV and CAPVCD

--- internal/feature/providers.go
+++ internal/feature/providers.go
@@ -1,9 +1,12 @@
 package feature
 
 const (
-	ProviderAWS       = "aws"
-	ProviderAzure     = "azure"
-	ProviderKVM       = "kvm"
-	ProviderOpenStack = "openstack"
-	ProviderCAPA      = "capa"
+	ProviderAWS           = "aws"
+	ProviderAzure         = "azure"
+	ProviderKVM           = "kvm"
+	ProviderOpenStack     = "openstack"
+	ProviderCAPA          = "capa"
+	ProviderCAPZ          = "capz"
+	ProviderCloudDirector = "cloud-director"
+	ProviderVSphere       = "vspshere"
 )
--- internal/key/common.go
+++ internal/key/common.go
@@ -15,6 +15,15 @@ func ClusterID(getter LabelsGetter) string {
 	return getter.GetLabels()[label.Cluster]
 }
 
+func MachineDeploymentName(getter AnnotationsGetter) string {
+	annotations := getter.GetAnnotations()
+	if annotations == nil {
+		return ""
+	}
+
+	return annotations[annotation.MachineDeploymentName]
+}
+
 func MachinePoolName(getter AnnotationsGetter) string {
 	annotations := getter.GetAnnotations()
 	if annotations == nil {
