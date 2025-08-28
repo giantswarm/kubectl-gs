bfecaf79e651e0f0a215614df628df437b991ddb
Marian Steinbach <marian@giantswarm.io>
2025-08-28 12:33:52 +0200

    Remove unused constants (#1729)
    * Remove unused ProviderVSphere
    
    * Remove unused ProviderCloudDirector
    
    * Remove unused ProviderCAPZ
    
    * Remove unused ProviderOpenStack
    
    * Remove unused ProviderCAPA

--- internal/feature/providers.go
+++ internal/feature/providers.go
@@ -1,10 +1,6 @@
 package feature
 
 const (
-	ProviderAWS           = "aws"
-	ProviderAzure         = "azure"
-	ProviderCAPA          = "capa"
-	ProviderCAPZ          = "capz"
-	ProviderCloudDirector = "cloud-director"
-	ProviderVSphere       = "vspshere" // TODO: fix spelling
+	ProviderAWS   = "aws"
+	ProviderAzure = "azure"
 )
