97cb1a601751d6d716aa8eed8c46b981ef81d710
taylorbot <dev@giantswarm.io>
2026-01-16 10:47:22 +0000

    Bump version to 4.10.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kubectl-gs"
 	source      = "https://github.com/giantswarm/kubectl-gs"
-	version     = "4.10.0"
+	version     = "4.10.1-dev"
 )
 
 func Description() string {
