d579227a49af9ba8585fcf7c0153eb338f332e51
Marian Steinbach <marian@giantswarm.io>
2026-03-06 13:36:38 +0100

    Add deploy chart command

---
+++ cmd/deploy/chart/flag.go
@@ -0,0 +1,119 @@
+package chart
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/giantswarm/microerror"
+	"github.com/spf13/cobra"
+)
+
+// semverRe matches a basic semver string like "1.2.3" or "v1.2.3".
+var semverRe = regexp.MustCompile(`^v?\d+\.\d+\.\d+$`)
+
+const (
+	flagChartName    = "chart-name"
+	flagOrganization = "organization"
+	flagCluster      = "cluster"
+	flagTargetNS     = "target-namespace"
+	flagOCIURLPrefix = "oci-url-prefix"
+	flagVersion      = "version"
+	flagValuesFile   = "values-file"
+	flagName         = "name"
+	flagAutoUpgrade  = "auto-upgrade"
+	flagInterval     = "interval"
+
+	defaultOCIURLPrefix = "oci://gsoci.azurecr.io/charts/giantswarm/"
+	defaultInterval     = "10m"
+)
+
+var validAutoUpgradeValues = []string{"all", "minor", "patch"}
+
+type flag struct {
+	ChartName    string
+	Organization string
+	Cluster      string
+	TargetNS     string
+	OCIURLPrefix string
+	Version      string
+	ValuesFile   string
+	Name         string
+	AutoUpgrade  string
+	Interval     string
+}
+
+func (f *flag) Init(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&f.ChartName, flagChartName, "", "Name of the chart to deploy.")
+	cmd.Flags().StringVar(&f.Organization, flagOrganization, "", "Giant Swarm organization name owning the target cluster.")
+	cmd.Flags().StringVar(&f.Cluster, flagCluster, "", "Target cluster name.")
+	cmd.Flags().StringVar(&f.TargetNS, flagTargetNS, "", "Target namespace in the workload cluster.")
+	cmd.Flags().StringVar(&f.OCIURLPrefix, flagOCIURLPrefix, defaultOCIURLPrefix, "OCI URL prefix for the chart registry.")
+	cmd.Flags().StringVar(&f.Version, flagVersion, "", "Chart version to deploy. If not specified, no version ref is set.")
+	cmd.Flags().StringVar(&f.ValuesFile, flagValuesFile, "", "Path to a YAML file with chart values.")
+	cmd.Flags().StringVar(&f.Name, flagName, "", "Override the resource name (default: <cluster>-<chart-name>).")
+	cmd.Flags().StringVar(&f.AutoUpgrade, flagAutoUpgrade, "", "Auto-upgrade strategy: all, minor, or patch.")
+	cmd.Flags().StringVar(&f.Interval, flagInterval, defaultInterval, "Reconciliation interval for OCIRepository and HelmRelease.")
+}
+
+func (f *flag) Validate() error {
+	if f.ChartName == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagChartName)
+	}
+	if strings.Contains(f.ChartName, "/") {
+		return microerror.Maskf(invalidFlagError, "--%s must not contain '/'", flagChartName)
+	}
+	if f.Organization == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagOrganization)
+	}
+	if f.Cluster == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagCluster)
+	}
+	if f.TargetNS == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagTargetNS)
+	}
+
+	if f.Version != "" && !semverRe.MatchString(f.Version) {
+		return microerror.Maskf(invalidFlagError, "--%s must be a valid semver version (e.g. 1.2.3), got %q", flagVersion, f.Version)
+	}
+
+	if f.AutoUpgrade != "" {
+		valid := false
+		for _, v := range validAutoUpgradeValues {
+			if f.AutoUpgrade == v {
+				valid = true
+				break
+			}
+		}
+		if !valid {
+			return microerror.Maskf(invalidFlagError, "--%s must be one of: %s", flagAutoUpgrade, strings.Join(validAutoUpgradeValues, ", "))
+		}
+		if f.AutoUpgrade != "all" && f.Version == "" {
+			return microerror.Maskf(invalidFlagError, "--%s=%s requires --%s to be set", flagAutoUpgrade, f.AutoUpgrade, flagVersion)
+		}
+	}
+
+	// Normalize OCI URL prefix.
+	f.OCIURLPrefix = normalizeOCIURLPrefix(f.OCIURLPrefix)
+
+	// Validate resource name length.
+	resourceName := f.Name
+	if resourceName == "" {
+		resourceName = fmt.Sprintf("%s-%s", f.Cluster, f.ChartName)
+	}
+	if len(resourceName) > 253 {
+		return microerror.Maskf(invalidFlagError, "resource name %q exceeds maximum length of 253 characters", resourceName)
+	}
+
+	return nil
+}
+
+func normalizeOCIURLPrefix(prefix string) string {
+	if !strings.HasPrefix(prefix, "oci://") {
+		prefix = "oci://" + prefix
+	}
+	if !strings.HasSuffix(prefix, "/") {
+		prefix = prefix + "/"
+	}
+	return prefix
+}
---
+++ cmd/deploy/chart/runner.go
@@ -0,0 +1,103 @@
+package chart
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/yaml"
+
+	"github.com/giantswarm/kubectl-gs/v5/internal/deploychart"
+	"github.com/giantswarm/kubectl-gs/v5/internal/key"
+	"github.com/giantswarm/kubectl-gs/v5/pkg/commonconfig"
+)
+
+type runner struct {
+	commonConfig *commonconfig.CommonConfig
+	fileSystem   afero.Fs
+	flag         *flag
+	logger       micrologger.Logger
+	stdout       io.Writer
+	stderr       io.Writer
+}
+
+func (r *runner) Run(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	err := r.flag.Validate()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.run(ctx, cmd, args)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
+	resourceName := r.flag.Name
+	if resourceName == "" {
+		resourceName = fmt.Sprintf("%s-%s", r.flag.Cluster, r.flag.ChartName)
+	}
+
+	namespace := key.OrganizationNamespaceFromName(r.flag.Organization)
+
+	ociURL := r.flag.OCIURLPrefix + r.flag.ChartName
+
+	// Read values file if provided.
+	var values map[string]any
+	if r.flag.ValuesFile != "" {
+		data, err := afero.ReadFile(r.fileSystem, r.flag.ValuesFile)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		err = yaml.Unmarshal(data, &values)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	ociRepoOpts := deploychart.OCIRepositoryOptions{
+		Name:        resourceName,
+		Namespace:   namespace,
+		ClusterName: r.flag.Cluster,
+		URL:         ociURL,
+		Version:     r.flag.Version,
+		AutoUpgrade: r.flag.AutoUpgrade,
+		Interval:    r.flag.Interval,
+	}
+
+	helmReleaseOpts := deploychart.HelmReleaseOptions{
+		Name:            resourceName,
+		Namespace:       namespace,
+		ClusterName:     r.flag.Cluster,
+		ChartName:       r.flag.ChartName,
+		TargetNamespace: r.flag.TargetNS,
+		Interval:        r.flag.Interval,
+		Values:          values,
+	}
+
+	ociRepo := deploychart.BuildOCIRepository(ociRepoOpts)
+	helmRelease := deploychart.BuildHelmRelease(helmReleaseOpts)
+
+	ociRepoYAML, err := deploychart.MarshalCRD(ociRepo)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	helmReleaseYAML, err := deploychart.MarshalCRD(helmRelease)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	fmt.Fprintf(r.stdout, "%s---\n%s", string(ociRepoYAML), string(helmReleaseYAML))
+
+	return nil
+}
